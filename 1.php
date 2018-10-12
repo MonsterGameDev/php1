a5b39fb7faf3a86edcd3248e7fb6427c01c76383
Per Høyer <per@brf.dk>
2018-10-12 15:57:51 +0200

    design v1

--- 1.php
+++ 1.php
@@ -1,17 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
+
 <body>
-    <h1><?php echo "Lars Erik er sød" ?></h1> 
-    <h2><?php echo "... og lugter godt!" ?></h2>
-    <hr>
-    <a href="/">Til Index</a>
+    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">PH´s PHP-site</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./1.php" class="nav-link">Side 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./phpinof.php" class="nav-link">PHP INFO</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <main class="container" style="margin-top: 75px">
+        <h1>Side 1</h1>
+    </main>
+
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+
 </body>
+
 </html>
---
+++ phpinof.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">PH´s PHP-site</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./1.php" class="nav-link">Side 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./phpinof.php" class="nav-link">PHP-INFO</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <main class="container" style="margin-top: 75px">
+        <h1>PHP Info Page</h1>
+    </main>
+
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+
+</body>
+
+</html>
