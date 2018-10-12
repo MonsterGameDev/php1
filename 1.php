92a00de073d352ac6edb24fe19968b0714ad54b9
Per Høyer <per@brf.dk>
2018-10-12 14:20:41 +0200

    message for lal

--- 1.php
+++ 1.php
@@ -1,6 +1,14 @@
-<?php
-
-$message = "Hello World";
-
-echo $message
-?>
+<DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1><?php echo Lars er sød</h1>
+</body>
+</html>
