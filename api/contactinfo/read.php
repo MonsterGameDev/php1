920f3b7caa564a74a78ee73f6c60dfaa7b95a856
Per Høyer <per@brf.dk>
2018-10-14 15:31:22 +0200

    API read tryout

--- api/contactinfo/read.php
+++ api/contactinfo/read.php
@@ -19,28 +19,3 @@
     $num = $result->rowCount();
     
     echo "FOUND " . $num . " records";
-
-    //Check if any records
-    if ($num > 0) {
-        $returnArr = Array();
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            extract( $row);
-
-            $item = Array(
-                'id'=> $id,
-                'firstname' => $firstname,
-                'lastname' => $lastname,
-                'jobtitle' => $jobtitle,
-                'email' => $email,
-                'phone' => $phone,
-                'street' => $street,
-                'city' => $city,
-                'country' => $country 
-            );
-
-            array_push($returnArr, $item);
-        }
-        echo json_encode($returnArr);
-    } else {
-        echo json_encode (Array('message' => 'No contacts found'));
-    }
