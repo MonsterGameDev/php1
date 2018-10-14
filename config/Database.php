5348b8314eb9fa76c35485421de7ce3dab30c4a4
Per Høyer <per@brf.dk>
2018-10-14 15:17:33 +0200

    API read tryout

--- config/Database.php
+++ config/Database.php
@@ -34,6 +34,8 @@
                 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+                echo "this went well";
+
              } catch (PDOException $e) {
                  echo 'Connection Error: ' . $e.getMessage();
              }
