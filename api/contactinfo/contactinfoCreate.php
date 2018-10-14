7042b6f0cd7b681781dc9bac00f8137be7f2ea2a
Per Høyer <per@brf.dk>
2018-10-14 23:10:39 +0200

    finally working

--- api/contactinfo/contactinfoCreate.php
+++ api/contactinfo/contactinfoCreate.php
@@ -1,6 +1,6 @@
 <?php
  $data = file_get_contents('php://input');
- echo $data;
- $data1 = json_decode($data);
 
- echo  $data->firstname;
+ $data = json_decode($data);
+
+ echo "FirstName: " . $data->firstname;
