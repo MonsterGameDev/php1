a5ecfc93d44a11be8bdddf22140625c0bcae52d2
Per Høyer <per@brf.dk>
2018-10-14 11:30:03 +0200

    tryout db access

--- 1.php
+++ 1.php
@@ -35,20 +35,54 @@
         </div>
     </div>
     <main class="container" style="margin-top: 75px">
+    <?php
+        $ok = false;
+        if(isset($_POST["submit"])) {
+            $ok = true;
+
+            $username = $_POST['username'];
+            $gender = $_POST['gender'];
+            $color = $_POST['color'];
+            
+        if($ok) {
+            $db = mysqli_connect('localhost', 'root', '', 'ph-php');
+            
+            $sql = sprintf("INSERT INTO  users (name, gender, color) VALUES ('%s',' %s', '%s')",
+                mysqli_real_escape_string($db, $username),
+                mysqli_real_escape_string($db, $gender),
+                mysqli_real_escape_string($db, $color)
+            );
+
+            echo $sql;
+            
+            mysqli_query($db, $sql);
+            
+            mysqli_close($db);
+
+            echo "<p>User Added</p>";
+        }
+        
+    }
+    ?>
         <h1>User Form</h1>
         <hr>
         <form method="post" action="">
             <div class="form-group">
                 <label for="my-username">UserName:</label>
-                <input id="my-username" class="form-control" type="text">
+                <input id="my-username" name="username" class="form-control" type="text">
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input id="password" class="form-control" type="password">
+                <input id="password" name="password" class="form-control" type="password">
+            </div>
+            <div class="form-group">
+                <label>Gender</label>
+                <input class="form-control" value="male" name="gender" type="radio">Male<br>
+                <input class="form-control" value="female" name="gender" type="radio">Female
             </div>
             <div class="form-group">
                 <label for="favouriteColor">Favourite Color</label>
-                <select id="favouriteColor" class="form-control">
+                <select id="favouriteColor" name="color" class="form-control">
                     <option value="#f00">Red</option>
                     <option value="#0f0">Green</option>
                     <option value="#00f">Blue</option>
@@ -56,7 +90,7 @@
             </div>
             <div class="form-group">
                 <label for="language">Spoken Language</label>
-                <select id="language" class="form-control">
+                <select id="language" name="languages[]" multiple size="3" class="form-control">
                     <option value="en">English</option>
                     <option value="fr">Frensh</option>
                     <option value="da">Danish</option>
@@ -64,11 +98,12 @@
             </div>
             <div class="form-group">
                 <label for="Comments">Comments</label>
-                <textarea id="Comments" class="form-control" rows="6"></textarea>
+                <textarea id="Comments" name="comments" class="form-control" rows="6"></textarea>
             </div>
+
             <div class="btn-group">
-                <button class="btn btn-primary" type="submit">Submit</button>
-                <button class="btn btn-secondary" type="button">Cancel</button>
+                <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+                <input class="form-control" type="checkbox" name="tc" value="ok">I accept the T&C</button>
             </div>
         </form>
         
