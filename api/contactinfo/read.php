5afcf2cc82813f5d47976d9baa3be199e09fd5d3
Per Høyer <per@brf.dk>
2018-10-14 15:28:35 +0200

    API read tryout

--- api/contactinfo/read.php
+++ api/contactinfo/read.php
@@ -7,18 +7,38 @@
     include_once './../../models/contactinfo.php';
 
     //Instantiate and connect
-try {
-   $database = new Database();
-   $db = $database->connect();
+    $database = new Database();
+    $db = $database->connect();
+
     //Instantiate Contactinfo - object
     $ci = new ContactInfo($db);
 
-    // Reading content
-    $result = $ci->read();
-    // Get rowcount
+    //Reading content
+    $result = $ci.read();
+    //Get rowcount
     $num = $result->rowCount();
 
-    echo $num;
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
+    //Check if any records
+    if ($num > 0) {
+        $returnArr = Array();
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract( $row);
+
+            $item = Array(
+                'id'=> $id,
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'jobtitle' => $jobtitle,
+                'email' => $email,
+                'phone' => $phone,
+                'street' => $street,
+                'city' => $city,
+                'country' => $country 
+            );
+
+            array_push($returnArr, $item);
+        }
+        echo json_encode($returnArr);
+    } else {
+        echo json_encode (Array('message' => 'No contacts found'));
+    }
